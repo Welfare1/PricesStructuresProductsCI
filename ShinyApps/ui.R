f07b696ad2481742a43f63864f8ee198013253eb
amedbah <amedbah2000@gmail.com>
2024-05-10 00:45:16 +0000

    Commit shinyApp 10/05/2024

--- ShinyApps/ui.R
+++ ShinyApps/ui.R
@@ -22,7 +22,110 @@ fluidPage(
       title = "",
       # Page 1 
       tabPanel(title = "Présentation",
-               "Page de présentation"),
+               fluidPage(
+                 tags$head(
+                   tags$style(
+                     HTML(
+                       "
+                            .body_row {
+                            
+                            height:410px;
+                            width:103%;
+                            margin-rigth:100px;
+                              background: linear-gradient(to bottom, #007bff, #ffffff); 
+                            }
+                            "
+                     )
+                   )
+                 ),
+                 
+                 fluidRow(
+                   #Head
+                   column(width = 3,
+                          img(src="img/logoUFHB.jpg", title="Popup", width = "50%"),
+                          img(src="img/logoUFR.jpg", title="Popup", width = "40%")
+                   ),
+                   column(width = 7,
+                          h2("ANALYSE DES PRIX  DES PRODUITS ALIMENTAIRES SUR LES MARCHES IVOIRIENS", 
+                             style = "color :orange;font-weight: bold;text-align:center"),
+                   ),
+                   
+                   column(width = 2,
+                          style="
+                                    display: inline-flex;
+                                    justify-content: space-evenly;",
+                          img(src="img/AmoirCI.png", title="Popup", width = "60%")
+                   )
+                   
+                 ),
+                 fluidRow(
+                   #BODY
+                   class="body_row",
+                   column(width = 1,),
+                   column(width = 4,
+                          
+                          br(), br(), br(),
+                          h4("#LUTTE CONTRE LA CHERTE DE LA VIE", style = "color : black;font-weight: bold;text-align:center"),
+                          br(),
+                          p("Ce projet s’inspire des directives de l’Etat de Côte d’ivoire dans
+                                  la lutte contre la cherté de la vie.", 
+                            style = "color : orange;font-size: 20px;font-weight: bold ;text-align:center"),
+                          br(),
+                          p("Il offre un outil de restitution des prix des différentes
+                                  denrées alimentaires dans les marchés, ainsi qu’une analyse portant sur leurs structures.", 
+                            style = "color : orange;font-size: 20px; font-weight: bold;text-align:center")
+                   ),
+                   
+                   # colonne 2
+                   column(width =7,
+                          style="
+                                    display: inline-flex;
+                                    justify-content: space-evenly;
+                                    width: 50%;",
+                          img(src="img/marche.png", title="Popup", width="70%")
+                   ),
+                   
+                 ),
+                 fluidRow(
+                   #Footer
+                   
+                   class="footer",
+                   column(width = 4,
+                          fluidRow(
+                            column(width = 6,
+                                   p("BAH AMADOU ALI",style = "color : black;font-size:20px;font-weight: bold;font-style: italic;"),
+                                   p("AKADJE FREDERIC",style = "color : black;font-size: 20px;font-weight: bold;font-style: italic; "),
+                            ),
+                            column(width = 5,
+                                   
+                                   img(src="img/LogoLinkedin.png", title="Popup", width = "20%"),
+                                   
+                                   img(src="img/logoGithub.png", title="Popup", width = "20%"),
+                                   
+                                   img(src="img/logoGmail.png", title="Popup", width = "20%"),
+                                   br(),
+                                   img(src="img/LogoLinkedin.png", title="Popup", width = "20%"),
+                                   
+                                   img(src="img/logoGithub.png", title="Popup", width = "20%"),
+                                   
+                                   img(src="img/logoGmail.png", title="Popup", width = "20%"),
+                                   
+                            ),
+                            column(width = 1,)
+                            
+                          )),
+                   
+                   column(width = 8,
+                          style="display: flex;
+                                    justify-content: space-between;",
+                          img(src="img/logoOpendata.png", title="Popup", width = "20%"),
+                          img(src="img/logoOcpv.png", title="Popup", width = "20%"),
+                          img(src="img/logoCnlvc.png", title="Popup", width = "20%"),
+                          img(src="img/logoSikaF.png", title="Popup", width = "20%")
+                   )
+                   
+                 ),
+               )),
       
       # Page 2
       tabPanel(title = "Analyse par région",
