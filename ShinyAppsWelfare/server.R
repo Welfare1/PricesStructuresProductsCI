de0765af86eefc42ece629e249e9bf5b21a0469d
welfare <akadje37@gmail.com>
2024-05-21 09:47:11 +0000

    Chargement de shinywelfare

--- ShinyAppsWelfare/server.R
+++ ShinyAppsWelfare/server.R
@@ -4,30 +4,159 @@
 function(input, output, session) {
   
   
+  #################################################################################
+  # SIDEBAR
+  ## Met à jour les choix des sous-catégories en fonction de la catégorie sélectionnée
+  observeEvent(input$wel_categorie, {
+    ### Filtrer les produits en fonction de la catégorie sélectionnée
+    if(input$wel_categorie=="TOUT"){
+      filtered_products <- DatasetPriceInit |>
+        select(`SOUS-CATEGORIE`)
+    }else{
+      filtered_products <- DatasetPriceInit |> 
+        filter(CATEGORIE==input$wel_categorie) |>
+        select(`SOUS-CATEGORIE`)
+    }
+    
+    ### Mettre à jour les choix du selectizeInput des produits
+    ### Vecteur unique des produits filtrés
+    UnivectProd <- c("TOUT",unique(filtered_products))
+    updateSelectizeInput(session,
+                         "wel_SousCat",
+                         selected = UnivectProd[1],
+                         choices = UnivectProd
+                         )
+  })
+
+  ## Met à jour les choix des sous-catégories en fonction de la catégorie sélectionnée
+  observeEvent(input$wel_SousCat, {
+    ### Filtrer les produits en fonction de la catégorie sélectionnée
+    if(input$wel_SousCat=="TOUT"){
+      filtered_products <- DatasetPriceInit |>
+        select(SPECIFICITE)
+    }else{
+      filtered_products <- DatasetPriceInit |>
+        filter(`SOUS-CATEGORIE`==input$wel_SousCat) |>
+        select(SPECIFICITE)
+    }
+
+    ### Mettre à jour les choix du selectizeInput des produits
+    ### Vecteur unique des produits filtrés
+    UnivectProd <- c("TOUT",unique(filtered_products))
+    updateSelectizeInput(session,
+                         "wel_Specificite",
+                         selected = UnivectProd[1],
+                         choices = UnivectProd
+    )
+  })
+  
+  ## Met à jour les choix des sous-catégories en fonction de la catégorie sélectionnée
+  observeEvent(input$wel_Specificite, {
+    ### Filtrer les produits en fonction de la catégorie sélectionnée
+    if(input$wel_Specificite=="TOUT"){
+      filtered_products <- DatasetPriceInit |>
+        select(PRODUITS)
+    }else{
+      filtered_products <- DatasetPriceInit |>
+        filter(SPECIFICITE==input$wel_Specificite) |>
+        select(PRODUITS)
+    }
+    
+    ### Mettre à jour les choix du selectizeInput des produits
+    ### Vecteur unique des produits filtrés
+    UnivectProd <- c("TOUT",unique(filtered_products))
+    updateSelectizeInput(session,
+                         "wel_produit",
+                         selected = UnivectProd[1],
+                         choices = UnivectProd
+    )
+  })
+  
+  # Dataset première times series
+  
   DatasetPriceSeries <- reactive({
     DatasetPriceSeries <- DatasetPrice|>
+      filterOption(input$wel_ville,"VILLE") |> 
+      filterOption(input$wel_date,"ANNEE") |>
+      filterOption(input$wel_categorie,"CATEGORIE") |>
+      filterOption(input$wel_SousCat,"SOUS-CATEGORIE") |>
+      filterOption(input$wel_Specificite,"SPECIFICITE") |>
+      filterOption(input$wel_produit,"PRODUITS") |>
       group_by(DATE) |>
       summarise(PrixMoy=mean(PRIX,na.rm = TRUE),
                 VarMoy=mean(TauxVar,na.rm =TRUE),
                 VarMoyAbs=mean(abs(TauxVar),na.rm =TRUE)) |>
-      select(DATE,PrixMoy)
+      select(DATE,!!sym(renameIndicator(input$wel_indicateur)))
   })
+  
+  # Dataset contenant les valeurs min et max selon les indicateurs
+  
+  RankTableMinMax <- reactive({
+    RankTableMinMax <- DatasetPrice |>
+      filterOption(input$wel_ville,"VILLE") |>
+      filterOption(input$wel_date,"ANNEE") |>
+      filterOption(input$wel_categorie,"CATEGORIE") |>
+      filterOption(input$wel_SousCat,"SOUS-CATEGORIE") |>
+      filterOption(input$wel_Specificite,"SPECIFICITE") |>
+      group_by(DATE) |>
+      summarise(PrixMoy=mean(PRIX,na.rm = TRUE),
+                VarMoy=mean(TauxVar,na.rm =TRUE),
+                VarMoyAbs=mean(abs(TauxVar),na.rm =TRUE)) |>
+      select(DATE,!!sym(renameIndicator(input$wel_indicateur))) |> 
+      mutate(RankMin=min_rank(!!sym(renameIndicator(input$wel_indicateur))),
+             RankMax=min_rank(-!!sym(renameIndicator(input$wel_indicateur)))) |> 
+      filter(RankMin==1 | RankMax==1) |> 
+      arrange(-desc(RankMin)) |> 
+      distinct(RankMin,.keep_all = TRUE) |> 
+      distinct(RankMax,.keep_all = TRUE)
+  })
+  
+  # Dataset Times series cycliques
   
   DatasetPriceMois <- reactive({
     DatasetPriceMois <- DatasetPrice|>
-      ungroup()|>
+      filterOption(input$wel_ville,"VILLE") |>
+      filterOption(input$wel_categorie,"CATEGORIE") |>
+      filterOption(input$wel_SousCat,"SOUS-CATEGORIE") |>
+      filterOption(input$wel_Specificite,"SPECIFICITE") |>
+      group_by(ANNEE,MOISNum) |> 
+      summarise(PrixMoy=mean(PRIX,na.rm = TRUE),
+                VarMoy=mean(TauxVar,na.rm =TRUE),
+                VarMoyAbs=mean(abs(TauxVar),na.rm =TRUE)) |> 
       pivot_wider(id_cols = MOISNum,
                   names_from = ANNEE,
-                  values_from = PRIX,
-                  values_fn = ~ mean(.x, na.rm = TRUE)) 
+                  values_from = !!sym(renameIndicator(input$wel_indicateur))
+                  )
   })
+  
+  # Tables des indicateurs
+  DatasetSummInd <- reactive({
+    DatasetSummInd <- DatasetPrice|>
+      filterOption(input$wel_ville,"VILLE") |>
+      filterOption(input$wel_categorie,"CATEGORIE") |>
+      filterOption(input$wel_SousCat,"SOUS-CATEGORIE") |>
+      filterOption(input$wel_Specificite,"SPECIFICITE") |>
+      filter(CATEGORIE!="PRODUITS MANUFACTURES") |>
+      group_by(ANNEE,MOIS) |>
+      summarise(PrixMoy=mean(PRIX,na.rm = TRUE),
+                VarMoy=mean(TauxVar,na.rm =TRUE),
+                VarMoyAbs=mean(abs(TauxVar),na.rm =TRUE)) |>
+      pivot_wider(names_from =ANNEE,
+                  values_from = !!sym(renameIndicator(input$wel_indicateur)),
+                  id_cols = MOIS )
+  })
+  
   
   # Dataset contenant les meilleurs produits par saison
   ## récupération du classement des produits
   
   DatasetPriceRank <- reactive({
     indicateur1 <- DatasetPrice |>
-      # filter(ANNEE==2020) |>
+      filterOption(input$wel_ville,"VILLE") |>
+      filterOption(input$wel_date,"ANNEE") |>
+      filterOption(input$wel_categorie,"CATEGORIE") |>
+      filterOption(input$wel_SousCat,"SOUS-CATEGORIE") |>
+      filterOption(input$wel_Specificite,"SPECIFICITE") |>
       filter(CATEGORIE!="PRODUITS MANUFACTURES") |>
       mutate(MoisAn=as.yearmon(DATE),
              QuarterAn=as.yearqtr(DATE),
@@ -37,7 +166,7 @@ function(input, output, session) {
       summarise(PrixMoy=mean(PRIX,na.rm = TRUE),
                 VarMoy=mean(TauxVar,na.rm =TRUE),
                 VarMoyAbs=mean(abs(TauxVar),na.rm =TRUE)) |> 
-      mutate(Rank=min_rank(VarMoy)) |>
+      mutate(Rank=min_rank(!!sym(renameIndicator(input$wel_indicateur)))) |>
       filter(Rank==1)|> 
       ungroup() |>
       distinct(QuarterAn,.keep_all=TRUE)
@@ -45,10 +174,11 @@ function(input, output, session) {
   })
   
   # Table contenant les noms des produits et leur catégorie
-  
+
   ranKTable <- reactive({
     ranKTable <- left_join(SemestreTable,
-                           DatasetPriceRank() |> select(QuarterAn,`SOUS-CATEGORIE`),
+                           DatasetPriceRank() |> 
+                             select(QuarterAn,`SOUS-CATEGORIE`),
                            join_by(Quarter==QuarterAn),
                            keep = FALSE)
   })
@@ -72,7 +202,7 @@ function(input, output, session) {
   
   # Table recap page 3
   RankTableSai <- reactive({
-    RankTableSai <- priceGlobCleanFull |>
+    RankTableSai <-  DatasetPrice |>
       filter(CATEGORIE!="PRODUITS MANUFACTURES") |>
       ungroup() |> 
       mutate(MoisAn=as.yearmon(DATE),
@@ -96,27 +226,75 @@ function(input, output, session) {
     
   })
   
+  # Times series principales
   output$wel_dygraph <- renderDygraph({
     dygraph(DatasetPriceSeries(),
-            main = "Evolution du prix moyen") |> 
+            main = str_c(input$wel_indicateur," sur les différentes années")) |> 
       dyRangeSelector() |> 
-      dyOptions(stackedGraph = TRUE)
+      dyOptions(stackedGraph = TRUE) |> 
+      addMinMax(RankTableMinMax())
   })
   
+  # Times series secondaires
   output$wel_dygraphMois <- renderDygraph({
     dygraph(DatasetPriceMois(),
-            main = "Evolution du prix moyen par année") |> 
-      dyOptions(fillGraph = TRUE, fillAlpha = 0.4,colors = RColorBrewer::brewer.pal(1, "Dark2"))
+            main = str_c(input$wel_indicateur," par année")) |> 
+      dyOptions(fillGraph = TRUE,
+                fillAlpha = 0.4,
+                colors = RColorBrewer::brewer.pal(1, "Dark2"))
   })
   
   output$wel_dygraphSai <- renderDygraph({
-    DatasetPriceSeries() |> 
-    dygraph(main = "Produit par saison") |> 
+    DatasetPriceSeries() |>
+    dygraph(main = "Produit par saison") |>
       dyOptions(fillGraph = TRUE,
                 fillAlpha = 0.1,
-                colors = RColorBrewer::brewer.pal(1, "Dark2")) |> 
+                colors = RColorBrewer::brewer.pal(1, "Dark2")) |>
       addShadNot(ranKTable())
   })
+  
+  output$wel_tableSaiRecap1 <-  render_gt(
+    expr = DatasetSummInd() |> 
+      gt() |> 
+      opt_interactive(
+        use_filters = FALSE,
+        use_search = FALSE,
+        use_compact_mode = TRUE,
+        use_text_wrapping = FALSE
+      ) |> 
+      fmt_number(columns = c(`2020`,`2021`,`2022`)) |>
+      # Formatage
+      # 2020
+      text_transform(
+        locations = cells_body(
+          columns = `2020`,
+          rows = formatingtable(input$wel_indicateur,`2020`,`2021`)
+        ),
+        fn = function(x) paste(x,down_arrow)
+      ) |>
+      text_transform(
+        locations = cells_body(
+          columns = `2020`,
+          rows = !formatingtable(input$wel_indicateur,`2020`,`2021`)
+        ),
+        fn = function(x) paste(x,up_arrow)
+      ) |> 
+      # 2021
+      text_transform(
+        locations = cells_body(
+          columns = `2021`,
+          rows = formatingtable(input$wel_indicateur,`2021`,`2022`)
+        ),
+        fn = function(x) paste(x,down_arrow)
+      ) |>
+      text_transform(
+        locations = cells_body(
+          columns = `2021`,
+          rows = !formatingtable(input$wel_indicateur,`2021`,`2022`)
+        ),
+        fn = function(x) paste(x,up_arrow)
+      )
+  )
   
   
   output$wel_tableSai <-  render_gt(expr = DatasetPriceSai() |> 
